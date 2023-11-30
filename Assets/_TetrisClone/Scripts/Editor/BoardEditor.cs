1fcfb19f5babe32dc53e390d951c72e8445b2321
barnett-justin-p <barnett.justin.p@gmail.com>
2023-11-30 16:17:52 -0500

    Implemented board, also reorganized file structure

---
+++ Assets/_TetrisClone/Scripts/Editor/BoardEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Board))]
+public class BoardEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Board board = (Board)target;
+        if(GUILayout.Button("Redraw Board"))
+        {
+            board.DrawBoard();
+        }
+        
+        DrawDefaultInspector();
+    }
+}
